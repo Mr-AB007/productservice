ca174f3f731983de5653c14c88b1600a1f6e267b
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2025-11-13 00:13:31 +0530

    controller update

--- src/main/java/com/microservice/productservice/rest/ProductController.java
+++ src/main/java/com/microservice/productservice/rest/ProductController.java
@@ -1,12 +1,63 @@
 package com.microservice.productservice.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.microservice.productservice.payload.ProductRequest;
+import com.microservice.productservice.payload.response.ProductResponse;
+import com.microservice.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+@Log4j2
 public class ProductController {
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello Anubhav";
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest) {
+
+        log.info("Adding new product: {}", productRequest);
+
+        long productId = productService.addProduct(productRequest);
+        //return new ResponseEntity<>(productId, HttpStatus.CREATED);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productId);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable("id") long productId) {
+
+        log.info("ProductController | getProductById is called");
+
+        log.info("ProductController | getProductById | productId : {}", productId);
+
+        ProductResponse productResponse
+                = productService.getProductById(productId);
+        //return new ResponseEntity<>(productResponse, HttpStatus.OK);
+        return ResponseEntity.ok(productResponse);
+    }
+
+    @PutMapping("/reduceQuantity/{id}")
+    public ResponseEntity<Void> reduceQuantity(
+            @PathVariable("id") long productId,
+            @RequestParam long quantity
+    ) {
+
+        log.info("ProductController | reduceQuantity is called");
+
+        log.info("ProductController | reduceQuantity | productId : {}", productId);
+        log.info("ProductController | reduceQuantity | quantity : {}", quantity);
+
+        productService.reduceQuantity(productId,quantity);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProductById(@PathVariable("id") long productId) {
+        productService.deleteProductById(productId);
     }
 }
